ddcc8bf8842c518b8364cb451b65a36672e27f60
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-03-24 01:38:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  let elems = document.querySelectorAll('ul.ranked-list li');
+  for(let i=0; i < elems.length; i++) {
+    let num = parseInt(elems[i].innerHTML);
+    elems[i].innerHTML = (num + n).toString();
+  }
+}
+
+function deepestChild() {
+  
+}
